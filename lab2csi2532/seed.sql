f37a2ed5c6111dd48b986632394cf60476f30b06
Ahmed Ghadbawi <aghad14@uottawa.ca>
2022-01-31 13:21:07 -0500

    first push

---
+++ lab2csi2532/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO voitures
+VALUES('Toyota', 2020,'model F4'),
+  ('Toyota', 2013,'model SF2'),
+  ('Honda', 2012,'CIVIC'),
+  ('BMW', 2021,'model F4'),
+  ('Subaru', 2010,'model F5'),
+  ('Toyota', 2022,'cruiser');
